f1c16e0dae6f34789bf4fa93423c22a08e031c86
benwilcock <benwilcock@gmail.com>
2022-09-27 13:58:38 +0100

    added more attribs to REST endpoint

--- app.py
+++ app.py
@@ -3,9 +3,13 @@ import gunicorn
 
 app_name = "Python App With Flask, HTML, And REST"
 app = Flask(app_name)
+client = "VMware"
+framework = "Python with Pipenv"
 
 messages = [
     {
+        'client':client,
+        'framework':framework,
         'msg_subject':'Secure Software Supply Chains Are Great!', 
         'msg_body':'With A Secure Software Supply Chain, Deploying Python Code To PROD is Safe, Reliable, And Fast!'
     }
@@ -13,8 +17,7 @@ messages = [
 
 @app.route("/")
 def hello():
-    client = "VMware"
-    return render_template('index.html', client=client)
+    return render_template('index.html', client=client, framework=framework)
 
 @app.route('/messages')
 def get_incomes():
